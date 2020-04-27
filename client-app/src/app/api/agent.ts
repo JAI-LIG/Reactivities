f1bb8de43a9bba945be9fcd1e34b738c63128844
Jayriel Aguipo <jayriel_aguipo@ligph.com>
2020-04-27 18:52:28 +0800

    End of Section 7

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,6 +1,5 @@
 import { IActivity } from './../model/activity';
 import  axios, { AxiosResponse }  from 'axios';
-import { resolve } from 'dns';
 
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
@@ -11,10 +10,10 @@ const sleep = (ms: number) => (response: AxiosResponse) =>
     new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response),ms));
 
 const requests = {
-    get: (url: string) => axios.get(url).then(sleep(2000)).then(responseBody),
-    post: (url: string, body: {}) => axios.post(url, body).then(sleep(2000)).then(responseBody),
-    put: (url: string, body: {}) => axios.put(url, body).then(sleep(2000)).then(responseBody),
-    del: (url: string) => axios.delete(url).then(sleep(2000)).then(responseBody)
+    get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
+    post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
+    put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
+    del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody)
 }
 
 const Activities = {
--- client-app/src/feature/activities/form/ActivityForm.tsx
+++ client-app/src/feature/activities/form/ActivityForm.tsx
@@ -1,22 +1,20 @@
-import React, { useState, FormEvent } from "react";
+import React, { useState, FormEvent, useContext } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 import { IActivity } from "../../../app/model/activity";
 import {v4 as uuid} from 'uuid';
+import ActivityStore from '../../../app/stores/activityStore';
+import { observer } from "mobx-react-lite";
+
 
 interface IProps {
-  setEditMode: (editMode: boolean) => void;
   activity: IActivity;
-  createActivity: (createActivity: IActivity) => void;
-  editActivity: (activity: IActivity) => void;
-  submitting: boolean;
 }
 const ActivityForm: React.FC<IProps> = ({
-  setEditMode,
   activity: initialFormState,
-  createActivity,
-  editActivity,
-  submitting,
+
 }) => {
+  const activityStore = useContext(ActivityStore);
+  const {createActivity, editActivity, submitting, cancelOpenForm} = activityStore;
   const initializeForm = () => {
     if (initialFormState) {
       return initialFormState;
@@ -95,7 +93,7 @@ const ActivityForm: React.FC<IProps> = ({
         ></Form.Input>
         <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
         <Button
-          onClick={() => setEditMode(false)}
+          onClick={cancelOpenForm}
           floated='right'
           type='button'
           content='Cancel'
@@ -105,5 +103,5 @@ const ActivityForm: React.FC<IProps> = ({
   );
 };
 
-export default ActivityForm;
+export default observer(ActivityForm);
  
